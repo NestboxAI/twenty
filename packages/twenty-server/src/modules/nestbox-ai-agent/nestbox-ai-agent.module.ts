e71a6f197d6904eaae70f14b80ec462a94cd61f3
rishisoni1996 <sonirishi807@gmail.com>
2025-06-11 23:09:27 +0530

    wip(cron): stop cron

--- packages/twenty-server/src/modules/nestbox-ai-agent/nestbox-ai-agent.module.ts
+++ packages/twenty-server/src/modules/nestbox-ai-agent/nestbox-ai-agent.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { NestboxAiAgentCronCommand } from 'src/modules/nestbox-ai-agent/crons/commands/nestbox-ai-agent-start.command';
+import { NestboxAiAgentCronStopCommand } from 'src/modules/nestbox-ai-agent/crons/commands/nestbox-ai-agent-stop.command';
 import { NestboxAiAgentCronJob } from 'src/modules/nestbox-ai-agent/crons/jobs/nestbox-ai-agent.cron.job';
 
 @Module({
@@ -11,6 +12,7 @@ import { NestboxAiAgentCronJob } from 'src/modules/nestbox-ai-agent/crons/jobs/n
   ],
   providers: [
     NestboxAiAgentCronCommand,
+    NestboxAiAgentCronStopCommand,
     NestboxAiAgentCronJob,
   ],
   exports: [],
