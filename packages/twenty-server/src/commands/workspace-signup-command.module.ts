a39f50123fa137b6d29d14dd4210ae9395e1f10a
Roy Ganor <ganoro@gmail.com>
2025-08-02 21:15:42 -0400

    fix: bring back AuthModule

--- packages/twenty-server/src/commands/workspace-signup-command.module.ts
+++ packages/twenty-server/src/commands/workspace-signup-command.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ApiKeyModule } from 'src/engine/core-modules/api-key/api-key.module';
+import { AuthModule } from 'src/engine/core-modules/auth/auth.module';
 import { DomainManagerModule } from 'src/engine/core-modules/domain-manager/domain-manager.module';
 import { User } from 'src/engine/core-modules/user/user.entity';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
@@ -13,6 +14,7 @@ import { WorkspaceSignupCommand } from './workspace-signup.command';
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Workspace], 'core'),
+    AuthModule,
     ApiKeyModule,
     WorkspaceModule,
     DomainManagerModule,
